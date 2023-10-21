c5821b26b115309ce33fe73002a33e82f55e790b
Luong Luu <luongluu2001@gmail.com>
2023-10-21 19:01:41 -0400

    delete
    delete

--- Code/TheLocalSpot/src/main/java/thelocalspot/application/data/repository/StatusRepository.java
+++
@@ -1,8 +0,0 @@
-package thelocalspot.application.data.repository;
-
-import thelocalspot.application.data.entity.Status;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StatusRepository extends JpaRepository<Status, Long> {
-
-}
