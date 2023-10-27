619e045eb7ffb5db9116dcf69d34e92b2bcb24c3
Jpelle25 <pellegrinojosh@gmail.com>
2023-10-26 20:39:14 -0400

    Creation of Fake Data Usage/Implementation | Bringing back Abstract Entity - needed usage

---
+++ Code/TheLocalSpot/target/classes/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO "GENUSER" (ID, VERSION, FIRST_NAME, LAST_NAME, EMAIL, ROLE, ADDRESS, ZIP_CODE, PHONE_NUMBER, PREFERENCES) VALUES
+(1, 1, 'John', 'A', 'test1@gmail.com', 'General User', '101 Cherry Lane', '12345', '+(111)-111-1111', 'Music,Gaming,Sports'),
+(2, 1, 'John', 'B', 'test2@gmail.com', 'General User', '102 Cherry Lane', '12345', '+(222)-222-2222', 'Sports'),
+(3, 1, 'John', 'C', 'test3gmail.com', 'General User', '103 Cherry Lane', '12345', '+(333)-333-3333', 'Recreational,Theatre'),
+(4, 1, 'John', 'D', 'test4@gmail.com', 'General User', '104 Cherry Lane', '12345', '+(444)-444-4444', 'Music,Comedy,Theatre,Gaming,Sports,Recreational');
