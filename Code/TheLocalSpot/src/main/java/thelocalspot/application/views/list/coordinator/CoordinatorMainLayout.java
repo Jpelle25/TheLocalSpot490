5043678c496066db85d10cb6dae170f7a368b20d
Luong Luu <luongluu2001@gmail.com>
2023-11-14 21:17:51 -0500

    coordinator package update
    coordinator package update

--- Code/TheLocalSpot/src/main/java/thelocalspot/application/views/list/coordinator/CoordinatorMainLayout.java
+++ Code/TheLocalSpot/src/main/java/thelocalspot/application/views/list/coordinator/CoordinatorMainLayout.java
@@ -1,53 +1,48 @@
 package thelocalspot.application.views.list.coordinator;
-
-import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.RouterLink;
-import com.vaadin.flow.server.VaadinServletRequest;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
-
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
+import thelocalspot.application.views.list.AfterAuthenticationView;
+import thelocalspot.application.views.list.genuser.TicketsView;
+import thelocalspot.application.views.list.genuser.UserEventsView;
+import thelocalspot.application.views.list.sharedfunctions.CheckAccountView;
 public class CoordinatorMainLayout extends AppLayout {
-
-    private static final String LOGOUT_SUCCESS_URL = "/";
-
     public CoordinatorMainLayout() {
-
         createHeader();
         createDrawer();
     }
-
     private void createHeader() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        OAuth2AuthenticatedPrincipal principal = (OAuth2AuthenticatedPrincipal) authentication.getPrincipal();
         H1 logo = new H1("The Local Spot");
         logo.addClassNames("text-l", "m-m");
-
-//        Button accountDetails = new Button("Account Details");
-//        accountDetails.addClickListener(buttonClickEvent -> accountDetails.getUI().ifPresent(ui -> ui.navigate(CheckAccountView.class)));
-        Button logoutButton = new Button("Logout", click -> {
-            UI.getCurrent().getPage().setLocation(LOGOUT_SUCCESS_URL);
-            SecurityContextLogoutHandler logoutHandler = new SecurityContextLogoutHandler();
-            logoutHandler.logout(
-                    VaadinServletRequest.getCurrent().getHttpServletRequest(), null,
-                    null);
-        });
-
-        HorizontalLayout header = new HorizontalLayout(new DrawerToggle(), logo, logoutButton);
-
+        Button accountDetails = new Button("Account Details");
+        accountDetails.addClickListener(buttonClickEvent -> accountDetails.getUI().ifPresent(ui -> ui.navigate(CheckAccountView.class)));
+        Button homeScreen = new Button("Home");
+        homeScreen.addClickListener(buttonClickEvent -> homeScreen.getUI().ifPresent(ui -> ui.navigate(AfterAuthenticationView.class)));
+        HorizontalLayout header = new HorizontalLayout(new DrawerToggle(), logo, accountDetails, homeScreen);
         header.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
         header.expand(logo);
         header.setWidthFull();
         header.addClassNames("py-0", "px-m");
-
         addToNavbar(header);
     }
-
     private void createDrawer() {
-        addToDrawer(
-                new RouterLink("Events", CoordinatorEventsView.class)
-        );
+        RouterLink pendingEvents = new RouterLink("Pending Events", PendingEventsView.class);
+        RouterLink approvedEvents = new RouterLink("Approved Events", ApprovedEventsView.class);
+        RouterLink deniedEvents = new RouterLink("Denied Events", DeniedEventsView.class);
+        addToDrawer(new VerticalLayout(
+                pendingEvents,
+                approvedEvents,
+                deniedEvents
+        ));
     }
 }
--- Code/TheLocalSpot/src/main/java/thelocalspot/application/views/list/coordinator/EventForm.java
+++ Code/TheLocalSpot/src/main/java/thelocalspot/application/views/list/coordinator/EventForm.java
@@ -1,18 +1,23 @@
 package thelocalspot.application.views.list.coordinator;
 import com.vaadin.flow.component.ComponentEvent;
 import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.combobox.MultiSelectComboBox;
 import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.notification.NotificationVariant;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.timepicker.TimePicker;
 import com.vaadin.flow.data.binder.BeanValidationBinder;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.shared.Registration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
@@ -28,17 +33,21 @@ import java.util.List;
 import java.util.Locale;
 public class EventForm extends FormLayout {
     TextField eventName = new TextField("Event Name");
-    MultiSelectComboBox<String> eventGenres = new MultiSelectComboBox<>("Event Genres");
+    ComboBox<String> eventGenres = new ComboBox<>("Event Genres");
     TimePicker eventTime = new TimePicker("Start Time");
     DatePicker dateStart = new DatePicker("Start Date");
     DatePicker dateEnd = new DatePicker("End Date");
+    TextField placeCapacity = new TextField("Place Capacity");
     TextField eventCapacity = new TextField("Event Capacity");
     TextField eventInfo = new TextField("Event Information");
     TextField maxTickets = new TextField("Number of Tickets");
     TextField ticketPrice = new TextField("Ticket Price");
     ComboBox<Host> host = new ComboBox<>("Host");
     ComboBox<Place> place = new ComboBox<>("Place");
-    Button finalize = new Button("Finalize");
+    Button createEvent = new Button("Create Event");
+    Button cancel = new Button("Cancel");
+    Button delete = new Button("Delete");
+    Button edit = new Button("Edit");
     EventService eventService;
     CoordUserService coordUserService;
     PlaceService placeService;
@@ -46,12 +55,13 @@ public class EventForm extends FormLayout {
         this.eventService = eventService;
         this.coordUserService = coordUserService;
         this.placeService = placeService;
-        List<CoordUser> test2 = coordUserService.getCoordUserEmail(principal.getAttribute("email"));
-        String[] test3 = test2.get(0).getCoordGenre().split(",");
-        eventGenres.setItems(test3);
+        List<CoordUser> coordUserSelf = coordUserService.getCoordUserEmail(principal.getAttribute("email"));
+        String[] selfGenre = coordUserSelf.get(0).getCoordGenre().split(",");
+        eventGenres.setItems(selfGenre);
         host.setItems(hosts);
         host.setItemLabelGenerator(Host::getHostName);
         place.setReadOnly(true);
+        placeCapacity.setReadOnly(true);
         host.addValueChangeListener(comboBoxHostComponentValueChangeEvent -> {
             place.setReadOnly(false);
             List<Place> placeTest = placeService.getHostId(host.getValue().getId());
@@ -62,11 +72,14 @@ public class EventForm extends FormLayout {
         Locale finnishLocale = new Locale("fi", "FI");
         dateStart.setLocale(finnishLocale);
         dateEnd.setLocale(finnishLocale);
-// eventGenres.setItems("Music", "Comedy", "Theatre", "Gaming", "Sports", "Recreational");
-        ticketPrice.setHelperText("Please enter in price with decimal point!");
         maxTickets.setMaxLength(6);
         dateStart.addValueChangeListener(e -> dateEnd.setMin(e.getValue()));
         dateEnd.addValueChangeListener(e -> dateStart.setMax(e.getValue()));
+        place.addValueChangeListener(comboBoxPlaceComponentValueChangeEvent -> {
+            placeCapacity.setValue(place.getValue().getPlaceCapacity());
+        });
+        createEvent.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        cancel.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
         add(
                 eventName,
                 eventGenres,
@@ -75,11 +88,13 @@ public class EventForm extends FormLayout {
                 eventTime,
                 dateStart,
                 dateEnd,
+                placeCapacity,
                 eventCapacity,
                 eventInfo,
                 maxTickets,
                 ticketPrice,
-                finalize
+                createEvent,
+                cancel
         );
     }
 }
