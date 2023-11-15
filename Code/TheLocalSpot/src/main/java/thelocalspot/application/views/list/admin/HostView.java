2b5c943eb7b7c791d50a60b04a0e6d86560e7fe4
Jpelle25 <pellegrinojosh@gmail.com>
2023-11-14 22:49:36 -0500

    Admin Implementation

--- Code/TheLocalSpot/src/main/java/thelocalspot/application/views/list/admin/HostView.java
+++ Code/TheLocalSpot/src/main/java/thelocalspot/application/views/list/admin/HostView.java
@@ -1,25 +1,56 @@
 package thelocalspot.application.views.list.admin;
 
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
 import jakarta.annotation.security.PermitAll;
+import thelocalspot.application.data.entity.GenUser;
+import thelocalspot.application.data.entity.Host;
+import thelocalspot.application.data.service.HostService;
+
+import java.util.List;
 
 @PageTitle("Admin")
 @Route(value = "admin-host", layout = AdminMainLayout.class)
-@PermitAll
+@AnonymousAllowed
 public class HostView extends VerticalLayout {
 
-    public HostView() {
+    Grid<Host> grid;
+    private final HostService hostService;
 
-        setSpacing(false);
-        add(new H2("This place intentionally left empty"));
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+    public HostView(HostService hostService) {
+        this.hostService = hostService;
         setSizeFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        getStyle().set("text-align", "center");
+        configureGrid();
+        add(getContent());
+        updateList();
+    }
+
+    private void configureGrid() {
+
+        grid = new Grid<>(Host.class);
+        grid.setSizeFull();
+        grid.setColumns("id", "firstName", "lastName", "email", "address", "zipCode", "phoneNumber");
+        grid.getColumns().forEach(col -> col.setAutoWidth(true));
+    }
+
+    private Component getContent() {
+        HorizontalLayout content = new HorizontalLayout(grid);
+        content.addClassName("content");
+        content.setSizeFull();
+        return content;
+    }
+
+    private void updateList() {
+
+        List<Host> hosts = hostService.getUserDetails();
+        grid.setItems(hosts);
     }
 }
